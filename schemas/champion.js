22d7908c7bf0075c910835759555af87a407dc00
mike14747 <mike4747@oh.rr.com>
2021-06-03 19:31:11 -0400

    Changed the double quotes in the new champion schema to single quotes.

--- schemas/champion.js
+++ schemas/champion.js
@@ -1,14 +1,14 @@
 export default {
-    name: "champion",
-    title: "Champion",
-    type: "document",
+    name: 'champion',
+    title: 'Champion',
+    type: 'document',
     fields: [
         {
-            name: "year",
-            title: "Year",
-            type: "date",
+            name: 'year',
+            title: 'Year',
+            type: 'date',
             options: {
-                dateFormat: "YYYY",
+                dateFormat: 'YYYY',
             },
             validation: (Rule) => Rule.required().custom(date => {
                 return date < '1978'
@@ -17,24 +17,24 @@ export default {
             }),
         },
         {
-            name: "championTeam",
-            title: "Champion Team",
-            type: "string",
+            name: 'championTeam',
+            title: 'Champion Team',
+            type: 'string',
         },
         {
-            name: "winnerManager",
-            title: "Champion Manager",
-            type: "string",
+            name: 'winnerManager',
+            title: 'Champion Manager',
+            type: 'string',
         },
         {
-            name: "runnerUpTeam",
-            title: "Runner Up Team",
-            type: "string",
+            name: 'runnerUpTeam',
+            title: 'Runner Up Team',
+            type: 'string',
         },
         {
-            name: "runnerUpManager",
-            title: "Runner Up Manager",
-            type: "string",
+            name: 'runnerUpManager',
+            title: 'Runner Up Manager',
+            type: 'string',
         },
     ],
 };
