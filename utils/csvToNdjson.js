8cf8a25a6447fb9da7dff5e66f2f5571ea9037f9
mike14747 <mike4747@oh.rr.com>
2021-06-10 00:49:34 -0400

    Cleaned up the code in /utils/csvToNdjson.js.

--- utils/csvToNdjson.js
+++ utils/csvToNdjson.js
@@ -1,43 +1,45 @@
 const parse = require('csv-parse');
 const fs = require('fs');
+const fsPromises = fs.promises;
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
+function appendToFile(rowData) {
+    const rowAsNdJson = `${JSON.stringify(rowData)}\n`
+    fs.appendFileSync(path.join(__dirname, '../data/events.ndjson'), rowAsNdJson);
+}
 
-const csvData = [];
-fs.createReadStream(path.join(__dirname, '../data/events.csv'))
-    .pipe(
-        parse({
-            delimiter: ',',
-            columns: true,
-            // from_line: 1,
-            // to_line: 2,
-            trim: true,
-            skip_empty_lines: true,
-            cast: true,
-        })
-    )
-    // each row being parsed is added to the csvData array
-    .on('data', (row => {
-        // console.log(row);
-        const rowData = {
-            _type: 'event',
-            _id: uuidv4(),
-            eventDate: row.eventDate,
-            event: row.event,
-            details: row.details,
-            active: row.active,
-        };
-        csvData.push(rowData);
-    }))
-    // abort if there is an error
-    .on('error', error => console.log('An error occured:', error))
-    .on('end', async function () {
-        try {
-            // console.log(csvData);
-            // write the csvData array to an .ndjson file
-            fs.open(path.join(__dirname, '../data/events.ndjson'), 'w', function (err, f) {
-                console.log('File was created and/or opened with write access!');
-            });
-        } catch { error => console.log(error) }
-    });
+function readCsvFile() {
+    fs.createReadStream(path.join(__dirname, '../data/events.csv'))
+        .pipe(
+            parse({
+                delimiter: ',',
+                columns: true,
+                trim: true,
+                skip_empty_lines: true,
+                cast: true,
+            })
+        )
+        .on('data', (row => {
+            const rowData = {
+                _type: 'event',
+                _id: uuidv4(),
+                eventDate: row.eventDate,
+                event: row.event,
+                details: row.details,
+                active: row.active.toLowerCase() == 'true' ? true : false,
+            };
+            appendToFile(rowData);
+        }))
+        .on('error', error => console.log('An error occured:', error));
+}
+
+async function openAndTruncateNdjsonFile() {
+    try {
+        await fsPromises.open(path.join(__dirname, '../data/events.ndjson'), 'w');
+        await fsPromises.truncate(path.join(__dirname, '../data/events.ndjson'), 0);
+        readCsvFile();
+    } catch { error => console.log(error) }
+}
+
+openAndTruncateNdjsonFile();
