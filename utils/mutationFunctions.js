a80bbd592d407f1d494f33995ce1fa49f2000e92
mike14747 <mike4747@oh.rr.com>
2021-06-11 21:46:24 -0400

    Added /utils/mutationFunction.js with a deleteAllEvents function.

---
+++ utils/mutationFunctions.js
@@ -0,0 +1,23 @@
+const mutationQueryUrl = 'https://tmt0dwwl.api.sanity.io/v1/data/mutate/production';
+
+async function deleteAllEvents() {
+    const mutations = [
+        {
+            delete: {
+                query: '*[_type == "event"]',
+            },
+        },
+    ];
+    await fetch(mutationQueryUrl, {
+        method: 'post',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({ mutations }),
+    }).then(res => console.log('Delete Response:', res)).catch(error => console.log(error));
+    return 'done';
+}
+
+module.exports = {
+    deleteAllEvents,
+}
